6d1ff804347d776d4f72f2d0415ffe2d742d6869
Jeerapas Kwansiri <jeerapas.kwansiri@gmail.com>
2026-02-25 09:08:18 +0700

    J: (refector) minor modify on config.py and call_function.py

--- call_function.py
+++ call_function.py
@@ -1,6 +1,7 @@
 # call_function.py
 
 from google.genai import types
+from config import WORKING_DIR
 from functions.get_files_info import schema_get_files_info, get_files_info
 from functions.get_file_content import schema_get_file_content, get_file_content
 from functions.write_file import schema_write_file, write_file
@@ -42,7 +43,7 @@ def call_function(function_call, verbose=False):
         )
     
     args = dict(function_call.args) if function_call.args else {}
-    args["working_directory"] = "./calculator"
+    args["working_directory"] = WORKING_DIR
     function_result = function_map[function_name](**args)
     
     return types.Content(
--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 # config.py
 
 # configuration in 'get_file_content.py'
-MAX_CHARS = 10000 # set maximum character for read() method 
+MAX_CHARS = 10000 # set maximum character for read() method
+
+# configuration in 'call_function.py'
+WORKING_DIR = "./calculator" # set working directory
