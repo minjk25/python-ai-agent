770d7844bd11c20abac83f930c727347bf7dd953
Jeerapas Kwansiri <jeerapas.kwansiri@gmail.com>
2026-02-26 02:50:03 +0700

    L: (refactor) add Rich library and update UX

--- call_function.py
+++ call_function.py
@@ -1,11 +1,15 @@
 # call_function.py
 
+from rich.console import Console
+from rich.panel import Panel
 from google.genai import types
 from config import WORKING_DIR
 from functions.get_files_info import schema_get_files_info, get_files_info
 from functions.get_file_content import schema_get_file_content, get_file_content
 from functions.write_file import schema_write_file, write_file
 from functions.run_python_file import schema_run_python_file, run_python_file
+
+console = Console()
 
 available_functions = types.Tool(
     function_declarations=[
@@ -16,22 +20,19 @@ available_functions = types.Tool(
     ],
 )
 
-def call_function(function_call, verbose=False):
-    if verbose:
-        print(f"\n> Calling function: {function_call.name}({function_call.args})")
-    else:
-        print(f"\n> Calling function: {function_call.name}")
-    
-    function_map = {
+function_map = {
         "get_files_info": get_files_info,
         "get_file_content": get_file_content,
         "write_file": write_file,
         "run_python_file": run_python_file
     }
+
+def call_function(function_call, verbose=False):
+    args = dict(function_call.args) if function_call.args else {}
+    function_name = function_call.name
+    mapped_function = function_map.get(function_name)
     
-    function_name = function_call.name or ""
-    
-    if function_name not in function_map:
+    if mapped_function is None:
         return types.Content(
             role="tool",
             parts=[
@@ -42,9 +43,36 @@ def call_function(function_call, verbose=False):
             ],
         )
     
-    args = dict(function_call.args) if function_call.args else {}
+    console.print(
+        Panel.fit(
+            f"[bold cyan]⚠ Agnet Calling function:[/bold cyan] [bold]{function_name}()[/bold]",
+        border_style="cyan"
+        )
+    )
+    
+    if verbose:
+        if function_name == "write_file":
+            file_path_value = args.get("file_path", "")
+            content_value = args.get("content", "")
+            console.print(f"[cyan]  ├─ file_path:[/cyan] [bold]{file_path_value}[/bold]")
+            console.print(f"[cyan]  ├─ content:[/cyan]")
+            console.print(
+                    Panel.fit(
+                        f"[#2b6f77]{content_value}[/#2b6f77]",
+                    border_style="#2b6f77"
+                    )
+                )
+        elif function_name == "run_python_file":
+            file_path_value = args.get("file_path", "")
+            args_value = args.get("args", "")
+            console.print(f"[cyan]  ├─ file_path:[/cyan] [bold]{file_path_value}[/bold]")
+            console.print(f"[cyan]  ├─ args:[/cyan] [bold]{args_value}[/bold]")
+        else:
+            for param, value in args.items():
+                console.print(f"[cyan]  ├─ {param}:[/cyan] [bold]{value}[/bold]")
+    
     args["working_directory"] = WORKING_DIR
-    function_result = function_map[function_name](**args)
+    function_result = mapped_function(**args)
     
     return types.Content(
         role="tool",
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import sys
 import argparse
 
-
+from rich.console import Console
+from rich.panel import Panel
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -10,7 +11,10 @@ from prompts import system_prompt
 from call_function import available_functions, call_function
 from config import MAX_ITERATIONS
 
+console = Console()
+
 def main():
+    
     parser = argparse.ArgumentParser(description="AI code Assistant.")
     parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini AI.")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
@@ -24,33 +28,47 @@ def main():
     
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
-    if args.verbose:
-        print(f"\nUser prompt: {args.user_prompt}\n")
+    
+    #if args.verbose:
+    print()
+    console.print(
+        Panel.fit(
+            f'[bold][#e7af47]󰁕 User prompt: [/#e7af47]"{args.user_prompt}"[/bold]',
+            border_style="#e7af47"
+        )
+    )
+    print()
     
     for _ in range(MAX_ITERATIONS):
         try:
             response_answer = generate_content(client, messages, args)
+            
             if response_answer:
-                print("\nResponse:")
-                print(response_answer)
+                console.print("[bold #e7af47]\n󰌵 Agent Response 󰌵[/bold #e7af47]")
+                console.print(
+                    Panel.fit(
+                        f"[bold cyan]{response_answer}[/bold cyan]",
+                    border_style="#e7af47"
+                    )
+                )
                 return
         except Exception as e:
-            print(f"Error in generate_content: {e}")
+            console.print(f"[bold red]Error in generate_content: {e}[/bold red]")
             return
     
-    print("Error: Maximum iterations reached without a final response.")
+    console.print("[bold red]Error: Maximum iterations reached without a final response.[/bold red]")
     sys.exit(1)
 
 def generate_content(client, messages, args):
-    response = client.models.generate_content(
-        model='gemini-2.5-flash', 
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt
+    with console.status("[bold green]Thinking...", spinner="dots"):
+        response = client.models.generate_content(
+            model='gemini-2.5-flash', 
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt
+            )
         )
-    )
     
     candidates = response.candidates
     if candidates:
@@ -64,16 +82,15 @@ def generate_content(client, messages, args):
             "The model may not have returned token usage information."
         )
     
-    if args.verbose:
-        print(" > Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print(" > Response tokens:", response.usage_metadata.candidates_token_count)
-        print(" > Total tokens:", response.usage_metadata.total_token_count)
-    
     function_calls = response.function_calls
     if function_calls:
         function_result_list = []
         for function_call in function_calls:
             function_call_result = call_function(function_call, args.verbose)
+            if args.verbose:
+                console.print(f"\n[#333539] ◇ Prompt tokens: {response.usage_metadata.prompt_token_count}[/#333539]")
+                console.print(f"[#2d3034] ◇ Response tokens: {response.usage_metadata.candidates_token_count}[/#2d3034]")
+                console.print(f"[#2d3034] ◇ Total tokens: {response.usage_metadata.total_token_count}[/#2d3034]\n")
             if not function_call_result.parts:
                 raise Exception("No 'parts' list in function call result")
             if not function_call_result.parts[0].function_response:
@@ -84,14 +101,31 @@ def generate_content(client, messages, args):
             function_result_list.append(function_call_result.parts[0])
             
             if args.verbose:
-                print("╰ Result:")
-                print(function_call_result.parts[0].function_response.response["result"])
-                print("-----------------------------------------------\n")
+                console.print("[#2b6f77]● Result:[/#2b6f77]")
+                console.print(
+                    Panel.fit(
+                        f"[#2b6f77]{function_call_result.parts[0].function_response.response["result"]}[/#2b6f77]",
+                    border_style="#2b6f77"
+                    )
+                )
+                print("\n-------------------------------------------------------------------------------\n")
         
         messages.append(types.Content(role="user", parts=function_result_list))
-
+    
     else:
+        if args.verbose:
+                console.print(
+                    Panel.fit(
+                        f"[bold cyan]⚠ Agnet preparing response ... [/bold cyan]",
+                    border_style="cyan"
+                    )
+                )
+                console.print(f"\n[#637182] ◇ Prompt tokens: {response.usage_metadata.prompt_token_count}[/#637182]")
+                console.print(f"[#637182] ◇ Response tokens: {response.usage_metadata.candidates_token_count}[/#637182]")
+                console.print(f"[#637182] ◇ Total tokens: {response.usage_metadata.total_token_count}[/#637182]")
         return response.text
+
+color = "#637182"
 
 if __name__ == "__main__":
     main()
